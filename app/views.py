1d41115280580e3bf61c52bf69ebae8b19838b0b
Kevin <kevin.d@me.com>
2014-08-16 16:39:07 +0100

    db
    Added db, fixed stuff.

--- app/views.py
+++ app/views.py
@@ -5,18 +5,18 @@ from forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Kevin' } # fake user
-    posts = [ # fake array pre-db
-        {
-            'author': { 'nickname': 'John' },
-            'body': 'Beautiful day in Portland!'
+    user = { 'nickname': 'Miguel' }
+    posts = [
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
         },
-        {
-            'author': { 'nickname': 'Susan' },
-            'body': 'The Avengers movie was so cool!'
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
         }
     ]
-    return render_template("index.html",
+    return render_template('index.html',
         title = 'Home',
         user = user,
         posts = posts)
@@ -27,6 +27,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
         return redirect('/index')
-    return render_template('login.html',
+    return render_template('login.html', 
         title = 'Sign In',
-        form = form)
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
